554bb1b415ca2b60e1f387b8dbd542a791350a2b
rivaldi <60965024+rivaldi1234@users.noreply.github.com>
2026-05-11 11:11:07 +0800

    Add migrations, models, relations, resource controllers for items & categories

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,15 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Item;
 
-class Category extends Model
-{
+class Item extends Model {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = [
+        'name',
+        'quantity',
+        'price',
+        'category_id',
+    ];
 
-    public function items() {
-        return $this->hasMany(Item::class);
+    public function category() {
+        return $this->belongsTo(Category::class);
     }
 }
